07aba70b6736c3c4f01ed3bda4eb07ebe6a7a0dd
Martin Kukli <mrtnkukli@gmail.com>
2022-07-24 18:37:05 +0300

    Checkpoint

---
+++ src/components/DashboardSidebar.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="border-t-2 border-r-2 border-white bg-primary"></div>
+</template>
+
+<script lang="ts" setup></script>
--- src/components/PageDashboard.vue
+++ src/components/PageDashboard.vue
@@ -1,8 +1,12 @@
 <template>
   <DashboardNav />
-  <router-view />
+  <div class="flex flex-1">
+    <DashboardSidebar class="w-52" />
+    <router-view />
+  </div>
 </template>
 
 <script lang="ts" setup>
   import DashboardNav from "@/components/DashboardNav.vue"
+  import DashboardSidebar from "@/components/DashboardSidebar.vue"
 </script>
