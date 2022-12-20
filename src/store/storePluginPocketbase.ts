4012c0ec6421d5bd134d19f6d39473ae8ce29132
Martin Kukli <mrtnkukli@gmail.com>
2022-12-20 22:21:50 +0200

    Add pocketbase

---
+++ src/store/storePluginPocketbase.ts
@@ -0,0 +1,8 @@
+import { PiniaPluginContext } from "pinia"
+import PocketBase from "pocketbase"
+
+const pb = new PocketBase(import.meta.env.VITE_POCKETBASE)
+
+export function storePocketbasePlugin(context: PiniaPluginContext) {
+  context.store.plugins.pb = pb
+}
--- src/types/Pinia.d.ts
+++ src/types/Pinia.d.ts
@@ -1,10 +1,12 @@
 import "pinia"
 import { Router } from "vue-router"
+import PocketBase from "pocketbase"
 
 declare module "pinia" {
   export interface PiniaCustomProperties {
     plugins: {
       router: Router
+      pb: PocketBase
     }
   }
 }
